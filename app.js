52e4d572cb26b3d9abb768248359b441b34cd5f4
Maycon Mendes <maycon.mendes@senior.com.br>
2022-07-22 20:43:22 -0300

    [FEAT] - Cria script javascript, configura, seleciona componentes html e cria função pageTransition.

---
+++ app.js
@@ -0,0 +1,36 @@
+/*
+    Selectors 
+*/
+
+const sections = document.querySelectorAll('.section');
+const sectionBtns = document.querySelectorAll('.controls');
+const sectionBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content')
+
+
+/*
+    Events
+*/
+
+
+
+/*
+    Functions
+*/
+function pageTransition() {
+    // Button click active class;
+    for(let i = 0; i < sectionBtn.length; i++) {
+        sectionBtn[i].addEventListener('click', function() {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+        })
+    }
+}
+
+
+
+/*
+    Calls
+*/ 
+pageTransition();
